acd4277f1def479104f4cf3eb337fea5b7f40ee7
betularass <betularas16@gmail.com>
2025-03-15 17:15:45 +0300

    OCAK1

---
+++ OCAK1/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.Intrinsics.Arm;
+
+Console.WriteLine("Hello, World!");
+
+
+//1.SORU
+
+// Microsoft tarafından geliştirilmiş bir dir. Temel olarak, nesne yönelimli ve güçlü bir tip sistemi olan bu dil, yazılım geliştirme süreçlerinde kullanılmaktadır.
+
+
+
+// 2.SORU
+
+/* Bir program çalışması için gerekli olan
+
+Derleyici
+Çalışma Zamanı(Çalışma Zamanı Ortamı) 
+Bellek(RAM)
+İşlemci(CPU) 
+Depolama (HDD/SSD) 
+İşletim Sistemi     */
+
+
+
+
+// 3.SORU 
+/*
+
+{ 
+    Console.Write("Adınızı girin: ");
+    string ad = Console.ReadLine();  // Kullanıcıdan giriş al
+    Console.WriteLine("Merhaba, " + ad + "!");  // Mesajı ekrana yazdır
+ }
+  
+*/
